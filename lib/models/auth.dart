848b7cba49e52bc09c8730ea3905387fb4877f05
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-28 20:13:04 +0530

    added auth sigin

---
+++ lib/models/auth.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_online/models/user.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  static const _apiEndpoint = 'https://todo-app-csoc.herokuapp.com';
+  static const _register = 'auth/register';
+  static const _login = 'auth/login';
+  static const _profile = 'auth/profile';
+  User? _user;
+  String? _token;
+  Future<void> signIn({
+    required String name,
+    required String email,
+    required String username,
+    required String password,
+  }) {
+    _user = User(name: name, email: email, username: username);
+    return http
+        .post(Uri.parse('$_apiEndpoint/$_register'),
+            body: json.encode({
+              'name': name,
+              'email': email,
+              'username': username,
+              'password': password,
+            }))
+        .then((response) {
+      final responseData = json.decode(response.body);
+      _token = responseData['token'];
+    });
+  }
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,6 @@
+class User {
+  String name;
+  String email;
+  String username;
+  User({required this.name, required this.email, required this.username});
+}
