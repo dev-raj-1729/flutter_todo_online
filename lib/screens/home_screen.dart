0e6b33e751c30414cde6ee7634c1b9bbbaae7294
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-06-29 16:05:55 +0530

    added ListView.builder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_todo_online/models/api.dart';
+import 'package:flutter_todo_online/widgets/todo_tile.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName = '/home';
@@ -6,10 +9,16 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final apiProvider = Provider.of<Api>(context);
     return Scaffold(
-      body: Center(
-        child: Text('home'),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('To-do list'),
       ),
+      body: ListView.builder(
+          itemCount: apiProvider.todos.length,
+          itemBuilder: (context, index) =>
+              TodoTile(apiProvider.todos[index].title)),
     );
   }
 }
