38a94bb08efb30bae3781d6e11264d2a07956c26
dev-raj-1729 <dev.raj.r1729@gmail.com>
2021-07-02 11:35:20 +0530

    added .fromMap contructor

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,4 +3,8 @@ class User {
   String email;
   String username;
   User({required this.name, required this.email, required this.username});
+  User.fromMap(Map<String, dynamic> map)
+      : this.name = map['name'],
+        this.email = map['email'],
+        this.username = map['username'];
 }
